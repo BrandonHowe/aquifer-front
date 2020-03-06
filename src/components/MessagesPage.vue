c271453dc8235ee8ff635d5bd5160a385aaf0c3b
xWafl <73sampleperson@gmail.com>
2020-03-06 17:01:15 -0500

    :hammer: Rebuilt with the heroku server.

--- src/components/MessagesPage.vue
+++ src/components/MessagesPage.vue
@@ -80,9 +80,9 @@
     const isOpen = ws => ws.readyState === ws.OPEN;
 
     // PRODUCTION
-    // const socket = new WebSocket("wss://aquifer-social.herokuapp.com");
+    const socket = new WebSocket("wss://aquifer-social.herokuapp.com");
     // DEV
-    const socket = new WebSocket("ws://localhost:5000");
+    // const socket = new WebSocket("ws://localhost:5000");
 
     export default {
         name: 'MessagesPage',
