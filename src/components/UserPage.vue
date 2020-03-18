e60c23df721f697f3a3843b1acbada9c19bd1a7c
xWafl <73sampleperson@gmail.com>
2020-03-18 19:46:35 -0400

    :sparkles: Added online/offline status (and to user page!)

--- src/components/UserPage.vue
+++ src/components/UserPage.vue
@@ -3,17 +3,56 @@
         <div class="userNameDiv">
             <h1 class="userName">{{ username }}<span class="usernum">#{{ usernum }}</span></h1>
             <div class="userUnderline"></div>
+            <p>Status: {{online}}</p>
         </div>
     </div>
 </template>
 
 <script>
     import "../assets/colorVars.css";
+    import {config} from "../assets/config.js";
+    import xhr from "xhr";
+
     export default {
         name: "UserPage",
         props: {
             username: String,
             usernum: String
+        },
+        data() {
+            return {
+                online: false,
+            }
+        },
+        methods: {
+            checkStatusPromise(username, usernum) {
+                return new Promise((resolve) => {
+                    xhr({
+                        method: "get",
+                        uri: config.serverUrl + "/userStatus/" + username + "/" + usernum,
+                        useXDR: true,
+                        headers: {
+                            "Content-Type": "application/json",
+                            "Access-Control-Allow-Origin": "*",
+                        }
+                    }, (err, resp, body) => {
+                        if (err) throw err;
+                        if (resp.statusCode !== 200) {
+                            console.log(resp.statusCode);
+                        }
+                        resolve(body);
+                    });
+                })
+            },
+            async checkStatus() {
+                const username = localStorage.getItem("username");
+                const usernum = Number(localStorage.getItem("usernum"));
+                return await this.checkStatusPromise(username, usernum);
+            },
+        },
+        // TODO: make online property real-time
+        async mounted() {
+            this.online = await this.checkStatus();
         }
     }
 </script>
